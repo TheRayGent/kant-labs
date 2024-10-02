8ea58e62c371e0454b77c7bd73498eeffd5d80f9
TheRayGent <egortitanpc@gmail.com>
2024-10-02 03:40:44 +0200

    Update main.cpp

--- lab-4/main.cpp
+++ lab-4/main.cpp
@@ -1,29 +1,57 @@
 #include <iostream>
 using namespace std;
 
-int main1(){
+int n2(){
     int n;
     cin >> n;
     switch (n){
-        case 1: cout << "Январь";
-        case 2: cout << "Февраль";
-        case 3: cout << "Март";
-        case 4: cout << "Апрель";
-        case 5: cout << "Май";
-        case 6: cout << "Июнь";
-        case 7: cout << "Июль";
-        case 8: cout << "Август";
-        case 9: cout << "Сентябрь";
-        case 10: cout << "Октябрь";
-        case 11: cout << "Ноябрь";
-        case 12: cout << "Декабрь";
-        default: cout << "Error";
+        case 1: 
+            cout << "January";
+            break;
+        case 2: 
+            cout << "February";
+            break;
+        case 3: 
+            cout << "March";
+            break;
+        case 4: 
+            cout << "April";
+            break;
+        case 5: 
+            cout << "May";
+            break;
+        case 6: 
+            cout << "June";
+            break;
+        case 7: 
+            cout << "July";
+            break;
+        case 8: 
+            cout << "August";
+            break;
+        case 9: 
+            cout << "September";
+            break;
+        case 10: 
+            cout << "October";
+            break;
+        case 11: 
+            cout << "November";
+            break;
+        case 12: 
+            cout << "December";
+            break;
+        default: 
+            cout << "Error";
     }
+    return 0;
 }
-int main2(){
+
+int n3(){
     short x;
     cin >> x;
     cout << (x==-1 ? "Negative number" : "Positive number"); //Poyasnenye.txt
+    return 0;
 }
 
 int main() {
@@ -33,8 +61,8 @@ int main() {
     if (a < b && b > c) cout << b+a-c;
     else if (b%c==0) cout << b/c-b;
     else cout << a*b-c;
-    cout << "2.\n";
-    main1();
+    cout << "\n2.\n";
+    n2();
     cout << "\n3.\n";
-    main2();
+    n3();
 }
