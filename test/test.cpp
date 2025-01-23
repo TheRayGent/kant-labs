499ec277e9a47d6a7c4e187dcbed8658d4ef4cd8
TheRayGent <egortitanpc@gmail.com>
2025-01-23 03:17:10 +0200

    test

---
+++ test/test.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <initializer_list>
+#include <string>
+using namespace std;
+
+class MyList{
+private:
+    struct Node{
+        int data;
+        Node * next;
+        Node * prev;
+    };
+public:
+    Node * sent;
+
+
+    // Adding a node to the end of a list
+    int append(int data){
+        Node * last = sent->prev;
+        Node * p = new Node;
+        p->data = data;
+        p->prev = last;
+        p->next = sent;
+        sent->prev = p;
+        last->next = p;
+        return p->data;
+    }
+
+    MyList(initializer_list<int> list = {}){
+        sent = new Node;
+        sent->next = sent;
+        sent->prev = sent;
+        if(list.size() != 0)
+            for(int value: list)
+                append(value);
+            
+    }
+
+    ~MyList(){
+        for(Node * i = sent->next; i!=sent;){
+            i = i->next;
+            delete i->prev;
+        }
+        delete sent;
+    } 
+
+    // Clearing the list of all elements (except the list sentinel)
+    void clear(){
+        for(Node * i = sent->next; i!=sent;){
+            i = i->next;
+            delete i->prev;
+        }
+        sent->next = sent;
+        sent->prev = sent;
+    }
+
+
+    string getList(){
+        string list = "";
+        list+="[";
+        for(Node * i = sent->next; i!=sent; i = i->next){
+            list+=to_string(i->data);
+            if(i->next != sent) 
+                list+=", ";
+        }
+        list+="]";
+        return list;
+    }
+
+    // Deleting a list node by pointer
+    void remove(Node * node){
+        if(node == sent) return;
+
+        node->prev->next = node->next;
+        node->next->prev = node->prev;
+        delete node;
+    }
+
+    
+};
+
+int main(){
+    MyList a;
+    cout << a.getList();
+}
